e0d112c7dc7f3cdf2d28027e6192b279d71743ef
lemonju <237393775@qq.com>
2015-06-08 13:22:29 +0800

    'xpc'

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var thinking = require("./thinking");
-thinking.init("web.json", __dirname); 
+var app = require("./thinking");
+var interceptor = require("./lib/interceptor"),
+    controller = require("./lib/controller"),
+    forbidden = require("./lib/forbidden"),
+    session = require("./lib/sessionManager");
+
+app.use(session).use(forbidden).use(interceptor).use(controller);
+
+app.use(function(next) {
+    this.response.getWriter().print("hi,nodejs!")
+});
+
+app.init("web.json", __dirname);
--- thinking.js
+++ thinking.js
@@ -1,12 +1,13 @@
 /* part = 模块引入
 --------------------------------------------*/
 var server = require("./lib/server"),
-    md5 = require("./lib/md5"),
-    interceptor = require("./lib/interceptor"),
-    controller = require("./lib/controller"),
+
     verification = require("./lib/verification"),
     qs = require('querystring'),
     print = require("./lib/print");
+
+var next = require("./lib/next");
+var fns = [];
 
 /* part = 私有部分
 --------------------------------------------*/
@@ -29,7 +30,7 @@ global.Module = {
     path: require("path"),
     log: require("./lib/log"),
     util: require("./lib/hutil"),
-    sessionManager: require("./lib/sessionManager").getInstance()
+    //sessionManager: require("./lib/sessionManager").getInstance()
 }
 
 function startServer() {
@@ -39,20 +40,7 @@ function startServer() {
         request.setParams(qs.parse(queryData));
         //注入模板
         response.setTemplate(print);
-        //注入session
-        lastTid = request.getCookie().tid;
-        if (!lastTid || (lastTid && !Module.sessionManager.getSession(lastTid))) {
-            tid = md5.hex_md5(Date.now().toString());
-            response.setCookie("tid", tid, Date.now() + CONFIG.sessionContext.expireTime, "/").flushCookie();
-            session = Module.sessionManager.createSession(tid);
-        } else {
-            session = Module.sessionManager.getSession(lastTid);
-        }
-        session.refreshLastTime();
-        request.setSession(session);
-        //执行拦截器
-        //输送数据到控制器
-        interceptor.intercept(request, response) && controller.execute(request, response)
+        new next(fns, request, response);
     })
 }
 
@@ -72,7 +60,14 @@ app.init = function(setFile, addr) {
     //初始化log
     Module.log.init(CONFIG.log);
     //初始化sessionManager
-    Module.sessionManager.init(CONFIG.sessionContext);
+    //Module.sessionManager.init(CONFIG.sessionContext);
     //启动服务器
     startServer()
 }
+
+
+app.use = function(fn) {
+    if(Module.util.isFunction(fn))
+        fns.push(fn)
+    return this;
+};
